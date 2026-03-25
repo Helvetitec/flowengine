778b2cf2378a2c5054de19941cc32be726e24760
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-03-25 11:08:20 -0300

    feat: Added broadcastContext to HasFlowRuns

--- src/Traits/HasFlowRuns.php
+++ src/Traits/HasFlowRuns.php
@@ -4,6 +4,7 @@ namespace Helvetitec\FlowEngine\Traits;
 
 use Helvetitec\FlowEngine\Models\FlowRun;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use LogicException;
 
 trait HasFlowRuns
 {
@@ -12,6 +13,13 @@ trait HasFlowRuns
         return $this->morphMany(FlowRun::class, 'subject');
     }
 
+    /**
+     * Starts the flow with the FlowClass.
+     * **Important:** This will not run the flow, but only create or set it!
+     *
+     * @param string $flowClass
+     * @return FlowRun
+     */
     public function startFlow(string $flowClass): FlowRun
     {
         return $this->flowRuns()->firstOrCreate(
@@ -25,8 +33,32 @@ trait HasFlowRuns
         );
     }
 
+    /**
+     * Starts the flow and runs it.
+     *
+     * @param string $flowClass
+     * @param mixed $input
+     * @param boolean $force
+     * @return void
+     */
     public function runFlow(string $flowClass, mixed $input = null, bool $force = false)
     {
         $this->startFlow($flowClass)->runFlow($input, $force);
+    }
+
+    /**
+     * Broadcasts the context for all flowruns of this model. This will add the fields and overwrites already existing ones.
+     *
+     * @param array $data
+     * @return void
+     */
+    public function broadcastContext(array $data): void
+    {
+        $this->flowRuns()->each(function ($flowRun) use ($data){
+            if(!$flowRun instanceof FlowRun){
+                throw new LogicException("Object is not of type FlowRun but ".$flowRun::class."!");
+            }
+            $flowRun->mergeContext($data)->persist();
+        });
     }
 }
